cc1735457a503adb2c9c7cee4c27834fbfb068f0
cav04 <alex>
2022-07-11 18:00:08 -0500

    code cleanup, added car init

--- src/long_testing.cpp
+++ src/long_testing.cpp
@@ -2,11 +2,13 @@
 
 #include "std_msgs/Header.h"
 #include "std_msgs/Float64.h"
+#include "std_msgs/Bool.h"
+
 #include "automotive_platform_msgs/SpeedMode.h"
 
 #include "pacmod_msgs/PositionWithSpeed.h"
 #include "pacmod_msgs/PacmodCmd.h"
-#include "std_msgs/Bool.h"
+
 
 #include "sensor_msgs/Imu.h"
 
@@ -14,13 +16,16 @@ int enabled_;
 
 int speed;
 
-
 ros::Publisher throttlePub;
+ros::Publisher brakePub;
+ros::Publisher gearPub;
+ros::Publisher enablePub;
 ros::Subscriber curAccelSub;
 ros::Publisher desAccelPub;
 
 // this may need to be tuned, I just guessed on the value
-#define CONSTANT__ACCEL_BOUNDRY 0.005
+// IF (-CONSTANT_ACCEL_BOUNDRY < [measured acceleration] < CONSTANT_ACCEL_BOUNDRY) THEN car is at zero acceleration (constant velocity)
+#define CONSTANT_ACCEL_BOUNDRY 0.005 // [-0.005, 0.005]
 
 float accel_cur;
 float accel_des = 0;
@@ -30,7 +35,7 @@ void curAccelCallback(const sensor_msgs::Imu::ConstPtr& msg) {
 }
 
 int isConstantVelocity(){
-    return ((accel_cur < CONSTANT__ACCEL_BOUNDRY) && (accel_cur > -1*CONSTANT__ACCEL_BOUNDRY));
+    return ((accel_cur < CONSTANT_ACCEL_BOUNDRY) && (accel_cur > -1*CONSTANT_ACCEL_BOUNDRY));
 }
 
 void publishThrottleCmd(float value){
@@ -42,16 +47,31 @@ void publishThrottleCmd(float value){
     throttleCmd.f64_cmd = value;
 
     throttlePub.publish(throttleCmd);
-    ROS_INFO("DEBUG: %f\n", value);
+    ROS_INFO("DEBUG: %f", value);
 }
 
-// puts throttle at 40% for half a second, then holds throttle at 20%
-void startCar(void){
-    publishThrottleCmd(0.4);
+// gives enable message to pacmod
+// lifts break of pacmod
+// set gear to forward in pacmod
+void CarInit(void){
+    std_msgs::Bool enableCmd;
+    enableCmd.data = true;
 
-    ros::Duration(0.5).sleep(); // sleep for half a second
+    enablePub.publish(enableCmd);
 
-    publishThrottleCmd(0.2);
+    pacmod_msgs::PacmodCmd brakeCmd;
+    brakeCmd.enable = false;
+    brakeCmd.f64_cmd = 0;
+    brakeCmd.ui16_cmd = 0;
+
+    brakePub.publish(brakeCmd);
+
+    pacmod_msgs::PacmodCmd shiftCmd;
+    shiftCmd.enable = true;
+    shiftCmd.f64_cmd = 0;
+    shiftCmd.ui16_cmd = 3;
+
+    gearPub.publish(shiftCmd);
 }
 
 int main(int argc, char** argv){
@@ -59,38 +79,53 @@ int main(int argc, char** argv){
     ros::NodeHandle n;
 
     throttlePub = n.advertise<pacmod_msgs::PacmodCmd>("as_rx/accel_cmd", 1);
+    brakePub = n.advertise<pacmod_msgs::PacmodCmd>("as_rx/brake_cmd", 1);
+    gearPub = n.advertise<pacmod_msgs::PacmodCmd>("as_rx/shift_cmd", 1);
+    enablePub = n.advertise<std_msgs::Bool>("as_rx/enable", 1);
+
     desAccelPub = n.advertise<std_msgs::Float64>("desAccel", 1);
 
     curAccelSub = n.subscribe<sensor_msgs::Imu>("imu/data", 1, curAccelCallback);
 
-    // start the car by giving it a big throttle for a short amount of time, then bring throttle to lesser position
+    // this is the throttle position that will be held until the car reaches constant velocity
     float ped_val = 0.4;
+    // this is the acceleration command passed to the low level longitude controller after the car reaches constant velocity
+    float accel_val = 0.3;
+
     int forked = 0;
 
     std_msgs::Float64 desaccel_msg;
     ros::Time startTime = ros::Time::now();
     ros::Rate loop_rate(30);
 
+    CarInit();
+
     while(ros::ok()){
         if(forked == 0){
+            // get current time
             ros::Time now = ros::Time::now();
             float time = now.toSec() - startTime.toSec();
-            if(time > 0.5){
-                ped_val = 0.3;
-            }
-            if(isConstantVelocity() && (time > 1)){
+
+            // check if the car is at a constant velocity after 5 seconds
+            if(isConstantVelocity() && (time > 5)){
+                // car has reached a constant velocity
                 forked = 1;
                 // pass off control to longitude controller
-                // roslaunch echo echo.launch
+                // roslaunch echo testecho.launch
                 if(fork() == 0){
-                    // TODO exec long controller
+                    // CHILD
+                    // launch low level longitude  controller
                     execlp("roslaunch", "roslaunch", "echo", "testecho.launch", NULL);
+                    ROS_INFO("Error: control should not reach here");
                 } else {
+                    // PARENT
                     //ros::Duration(0.5).sleep(); // sleep for half a second
-                    desaccel_msg.data = 0.3;
+                    desaccel_msg.data = accel_val;
                 } 
-            } 
-            publishThrottleCmd(ped_val);
+            } else {
+                // car has not reached a constant velocity 
+                publishThrottleCmd(ped_val);
+            }
         } else {
             desAccelPub.publish(desaccel_msg);
         }
