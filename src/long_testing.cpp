6020a04052626632e4ae4d4ee571c0fa597e0f79
cav04 <alex>
2022-07-26 17:09:40 -0500

    added tests for brake

--- src/long_testing.cpp
+++ src/long_testing.cpp
@@ -13,7 +13,12 @@
 #include "sensor_msgs/Imu.h"
 
 // THIS IS THE OPTION TO CHOOSE WHAT TEST TO ENABLE
-//#define constantaccel__
+// test1 car still, then given constant desired acceleration value
+// test2 car still, then given series of increasing desired acceleration values
+// test3 car still, then given series of increasing desired velocity values
+// test4
+// test5
+#define test2
 
 int enabled_;
 
@@ -23,16 +28,33 @@ ros::Publisher throttlePub;
 ros::Publisher brakePub;
 ros::Publisher gearPub;
 ros::Publisher enablePub;
+
 ros::Subscriber curAccelSub;
+ros::Subscriber curVelSub; 
+ros::Subscriber curGSub;
+
 ros::Publisher desAccelPub;
+ros::Publisher desVelPub;
 
 // this may need to be tuned, I just guessed on the value
 // IF (-CONSTANT_ACCEL_BOUNDRY < [measured acceleration] < CONSTANT_ACCEL_BOUNDRY) THEN car is at zero acceleration (constant velocity)
 #define CONSTANT_ACCEL_BOUNDRY 0.1 // [-0.1, 0.1]
 #define NUMACCELVALS 6
+#define NUMVELVALS 6
 
 float accel_cur;
 float accel_des = 0;
+
+float vel_cur;
+float G_val;
+
+void curVelCallback(const std_msgs::Float64::ConstPtr& msg) {
+	vel_cur = msg->data;
+}
+
+void curGCallback(const std_msgs::Float64::ConstPtr& msg){
+    G_val = msg->data;
+}
 
 void curAccelCallback(const std_msgs::Float64::ConstPtr& msg) {
 	accel_cur = (msg->data);	
@@ -88,24 +110,26 @@ int main(int argc, char** argv){
     enablePub = n.advertise<std_msgs::Bool>("as_rx/enable", 1);
 
     desAccelPub = n.advertise<std_msgs::Float64>("desAccel", 1);
+    desVelPub = n.advertise<automotive_platform_msgs::SpeedMode>("ssc/arbitrated_speed_commands", 1);
 
     curAccelSub = n.subscribe<std_msgs::Float64>("xAccelFilter/accel_x", 1, curAccelCallback);
-
-    // this is the throttle position that will be held until the car reaches constant velocity
-    float ped_val = 0.0;
-    // this is the acceleration command passed to the low level longitude controller after the car reaches constant velocity
-    float accel_val = 0.3;
-    float accel_vals[NUMACCELVALS] = {0.0, 0.2, 0.4, 0.6, 0.8, 1};
-
-    int forked = 0;
+    curGSub = n.subscribe<std_msgs::Float64>("/G", 1, curGCallback);
+    curVelSub = n.subscribe<std_msgs::Float64>("as_tx/vehicle_speed", 1, curVelCallback);
 
     std_msgs::Float64 desaccel_msg;
+    automotive_platform_msgs::SpeedMode desvel_msg;
+    
     ros::Time startTime = ros::Time::now();
     ros::Rate loop_rate(30);
 
     CarInit();
 
-    #ifdef constantaccel__
+#ifdef test1
+    // this is the throttle position that will be held until the car reaches constant velocity
+    float ped_val = 0.0;
+    // this is the acceleration command passed to the low level longitude controller after the car reaches constant velocity
+    float accel_val = 0.3;
+    int forked = 0;
     while(ros::ok()){
         if(forked == 0){
             // get current time
@@ -138,9 +162,17 @@ int main(int argc, char** argv){
         
         ros::spinOnce();
     }
-    #endif
+#endif
+
+#ifdef test2
+    // this is the throttle position that will be held until the car reaches constant velocity
+    float ped_val = 0.0;
+    float accel_vals[NUMACCELVALS] = {0.0, 0.2, 0.4, 0.6, 0.8, 1};
     float prevtime;
     int idx_accel = 0;
+    int interval = 3;
+    int forked = 0;
+
     while(ros::ok()){
         // get current time
         ros::Time now = ros::Time::now();
@@ -166,7 +198,7 @@ int main(int argc, char** argv){
             float delta_time = time - prevtime;
             desaccel_msg.data = accel_vals[idx_accel];
             
-            if(delta_time > 3){
+            if(delta_time > interval){
                 prevtime = time;
                 idx_accel = (idx_accel + 1) % NUMACCELVALS;
             }
@@ -174,6 +206,134 @@ int main(int argc, char** argv){
             desAccelPub.publish(desaccel_msg);
         }
     }
+#endif
+
+#ifdef test3
+    // this is the throttle position that will be held until the car reaches constant velocity
+    float ped_val = 0.0;
+    float vel_vals[NUMVELVALS] = {0.0, 4.0, 8.0, 12.0, 16.0, 20.0};
+    float prevtime;
+    int idx_vel = 0;
+    int interval = 5;
+    int forked = 0;
+
+    while(ros::ok()){
+        // get current time
+        ros::Time now = ros::Time::now();
+        float time = now.toSec() - startTime.toSec();
+        if(forked == 0){
+            if(isConstantVelocity() && (time > 5)){
+                forked = 1;
+                
+                if(fork() == 0){
+                    // CHILD
+                    // launch low level longitude  controller
+                    execlp("roslaunch", "roslaunch", "echo", "echo.launch", NULL);
+                    ROS_INFO("Error: control should not reach here, ya dun fucked up");
+                } else {
+                    // PARENT
+                    prevtime = time;
+                }
+            } else {
+                publishThrottleCmd(ped_val);    
+            }
+        } else {
+            // begin giving increasing constant accel values
+            float delta_time = time - prevtime;
+            desvel_msg.speed = vel_vals[idx_vel];
+            
+            if(delta_time > interval){
+                prevtime = time;
+                idx_vel= (idx_vel + 1) % NUMVELVALS;
+            }
+            
+            desVelPub.publish(desvel_msg);
+        }
+    }
+#endif
+
+#ifdef test4
+    float accel_val = 0.3;
+    float vel_HighCutoff = 15.0;
+    float vel_LowCutoff = 10.0;
+    int forked = 0;
+    while(ros::ok()){
+        if(forked == 0){
+            // get current time
+            ros::Time now = ros::Time::now();
+            float time = now.toSec() - startTime.toSec();
+
+            // check if the car is at a velocity after 5 seconds
+            if((time > 5) && (vel_cur > vel_HighCutoff)){
+                // car has reached cutoff velocity
+                forked = 1;
+                // pass off control to longitude controller
+                // roslaunch echo testecho.launch
+                if(fork() == 0){
+                    // CHILD
+                    // launch low level longitude  controller
+                    execlp("roslaunch", "roslaunch", "echo", "testecho.launch", NULL);
+                    ROS_INFO("Error: control should not reach here");
+                } else {
+                    // PARENT
+                    //ros::Duration(0.5).sleep(); // sleep for half a second
+                    desaccel_msg.data = -G_val;
+                } 
+            } else {
+                desaccel_msg.data = accel_val;
+                desAccelPub.publish(desaccel_msg);
+            }
+        } else {
+            if(vel_cur < vel_LowCutoff){
+                desaccel_msg.data = -accel_val;
+            }
+
+            desAccelPub.publish(desaccel_msg);
+        }
+        
+        ros::spinOnce();
+    }
+#endif
+
+#ifdef test5
+    float accel_val = 0.3;
+    float vel_Cutoff = 15.0;
+    int forked = 0;
+    while(ros::ok()){
+        if(forked == 0){
+            // get current time
+            ros::Time now = ros::Time::now();
+            float time = now.toSec() - startTime.toSec();
+
+            // check if the car is at a velocity after 5 seconds
+            if((time > 5) && (vel_cur > vel_Cutoff)){
+                // car has reached cutoff velocity
+                forked = 1;
+                // pass off control to longitude controller
+                // roslaunch echo testecho.launch
+                if(fork() == 0){
+                    // CHILD
+                    // launch low level longitude  controller
+                    execlp("roslaunch", "roslaunch", "echo", "testecho.launch", NULL);
+                    ROS_INFO("Error: control should not reach here");
+                } else {
+                    // PARENT
+                    //ros::Duration(0.5).sleep(); // sleep for half a second
+                    desaccel_msg.data = -accel_val;
+                } 
+            } else {
+                desaccel_msg.data = accel_val;
+                desAccelPub.publish(desaccel_msg);
+            }
+        } else {
+            desaccel_msg.data = -accel_val;
+
+            desAccelPub.publish(desaccel_msg);
+        }
+        
+        ros::spinOnce();
+    }
+#endif
 
     return 0;
 }
